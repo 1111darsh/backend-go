085f1274ce21f93fd9da24a46810a7b58919468d
1111darsh <1111darsh@gmail.com>
2023-12-07 20:56:53 +0530

    Create go application

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # backend-go
 This is simple go app
+```bash
+mkdir src/github.com/1111darsh/task-api
+cd src/github.com/1111darsh/task-api
+
+go run main.go
+go mod init github.com/111darsh/task-api
+
+```
---
+++ src/github.com/1111darsh/task-api/main.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+// Task struct represents a task
+type Task struct {
+	ID          int    `json:"id"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Done        bool   `json:"done"`
+}
+
+var tasks = []Task{
+	{ID: 1, Title: "Task 1", Description: "This is task 1", Done: false},
+	{ID: 2, Title: "Task 2", Description: "This is task 2", Done: false},
+}
+
+func getTasks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(tasks)
+}
+
+func getTask(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	taskID, err := strconv.Atoi(params["id"])
+	if err != nil {
+		http.Error(w, "Invalid task ID", http.StatusBadRequest)
+		return
+	}
+
+	for _, task := range tasks {
+		if task.ID == taskID {
+			json.NewEncoder(w).Encode(task)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(map[string]string{"error": "Task not found"})
+}
+
+func createTask(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var newTask Task
+	_ = json.NewDecoder(r.Body).Decode(&newTask)
+	newTask.ID = len(tasks) + 1
+	tasks = append(tasks, newTask)
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(newTask)
+}
+
+func updateTask(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	taskID, err := strconv.Atoi(params["id"])
+	if err != nil {
+		http.Error(w, "Invalid task ID", http.StatusBadRequest)
+		return
+	}
+
+	for index, task := range tasks {
+		if task.ID == taskID {
+			var updatedTask Task
+			_ = json.NewDecoder(r.Body).Decode(&updatedTask)
+			updatedTask.ID = taskID
+			tasks[index] = updatedTask
+			json.NewEncoder(w).Encode(updatedTask)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(map[string]string{"error": "Task not found"})
+}
+
+func deleteTask(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	taskID, err := strconv.Atoi(params["id"])
+	if err != nil {
+		http.Error(w, "Invalid task ID", http.StatusBadRequest)
+		return
+	}
+
+	for index, task := range tasks {
+		if task.ID == taskID {
+			tasks = append(tasks[:index], tasks[index+1:]...)
+			json.NewEncoder(w).Encode(map[string]bool{"result": true})
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(map[string]string{"error": "Task not found"})
+}
+
+func main() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/tasks", getTasks).Methods("GET")
+	router.HandleFunc("/tasks/{id}", getTask).Methods("GET")
+	router.HandleFunc("/tasks", createTask).Methods("POST")
+	router.HandleFunc("/tasks/{id}", updateTask).Methods("PUT")
+	router.HandleFunc("/tasks/{id}", deleteTask).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
